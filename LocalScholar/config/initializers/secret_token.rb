d4e15bfec8c97940603d6abf68c30caa3944fea8
MarwanNakhaleh <mnakhaleh@gmail.com>
2013-12-30 20:43:17 -0500

    empty project

---
+++ LocalScholar/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocalScholar::Application.config.secret_key_base = 'f1eb4079c4566ee71bbc2cd1c42c2e05eded15928e192a06e0ddb7d2eff2be48e1ecb1788b70baa6260dce4bcd13982ff91807d6450b5fa8789f5057aa2060e2'
---
+++ LocalScholar/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LocalScholar::Application.config.session_store :cookie_store, key: '_LocalScholar_session'
